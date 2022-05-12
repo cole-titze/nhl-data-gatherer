db354c614e482cc7b5b428b34420d690c1466297
colet <cole.titze@mines.sdsmt.edu>
2022-05-12 01:58:42 -0600

    Updated database to hold up to 3 decimal places and 3 digits in front of the decimal (in case of 100.0)

--- NhlGameDatabase/Tables/PredictedGame.sql
+++ NhlGameDatabase/Tables/PredictedGame.sql
@@ -4,9 +4,9 @@
     homeTeamName VARCHAR(MAX) NOT NULL,
     awayTeamName VARCHAR(MAX) NOT NULL,
     gameDate Datetime NOT NULL,
-    vegasHomeOdds DECIMAL(4,1) NOT NULL DEFAULT 0,
-    vegasAwayOdds DECIMAL(4,1) NOT NULL DEFAULT 0,
-    modelHomeOdds DECIMAL(4,1) NOT NULL DEFAULT 0,
-    modelAwayOdds DECIMAL(4,1) NOT NULL DEFAULT 0,
+    vegasHomeOdds DECIMAL(6, 3) NOT NULL DEFAULT 0,
+    vegasAwayOdds DECIMAL(6, 3) NOT NULL DEFAULT 0,
+    modelHomeOdds DECIMAL(6, 3) NOT NULL DEFAULT 0,
+    modelAwayOdds DECIMAL(6, 3) NOT NULL DEFAULT 0,
     PRIMARY KEY(id),
 );
