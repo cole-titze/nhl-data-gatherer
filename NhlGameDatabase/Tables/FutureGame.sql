091c3aac4e6d6b4165a48b0c78a2b6d7ed449809
Cole Titze <cole.titze@outlook.com>
2022-08-13 21:09:33 -0500

    Deleted old sql tables from project

--- NhlGameDatabase/Tables/FutureGame.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[FutureGame]
-(
-    id INT NOT NULL PRIMARY KEY,
-    homeTeamId INT NOT NULL,
-    awayTeamId INT NOT NULL,
-    gameDate Datetime NOT NULL,
-    FOREIGN KEY (homeTeamId) REFERENCES Team(id),
-    FOREIGN KEY (awayTeamId) REFERENCES Team(id),
-)
