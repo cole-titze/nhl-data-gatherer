44fe2551e78b41516db94d90504d3a09859cbe06
Cole <cole.titze@outlook.com>
2022-08-19 19:21:23 -0500

    Updated entities to match tables

--- Entities/Models/PredictedGame.cs
+++ Entities/Models/PredictedGame.cs
@@ -6,11 +6,18 @@
         public int homeTeamId { get; set; }
         public int awayTeamId { get; set; }
         public DateTime gameDate { get; set; }
-        public double vegasHomeOdds { get; set; }
-        public double vegasAwayOdds { get; set; }
+        public double bovadaVegasHomeOdds { get; set; }
+        public double bovadaVegasAwayOdds { get; set; }
+        public double myBookieVegasHomeOdds { get; set; }
+        public double myBookieVegasAwayOdds { get; set; }
+        public double pinnacleVegasHomeOdds { get; set; }
+        public double pinnacleVegasAwayOdds { get; set; }
+        public double betOnlineVegasHomeOdds { get; set; }
+        public double betOnlineVegasAwayOdds { get; set; }
+        public double bet365VegasHomeOdds { get; set; }
+        public double bet365VegasAwayOdds { get; set; }
         public double modelHomeOdds { get; set; }
         public double modelAwayOdds { get; set; }
-        public bool hasBeenPlayed { get; set; }
     }
 }
 
