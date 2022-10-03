074350bef67c4cb5aad4c1a079a9edef303dd59d
Cole Titze <cole.titze@outlook.com>
2022-10-02 22:10:34 -0500

    Added log loss table

---
+++ NhlGameDatabase/Tables/GameLogLoss.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [dbo].[GameLogLoss]
+(
+    id INT NOT NULL,
+    bovadaLogLoss FLOAT NOT NULL DEFAULT 0,
+    myBookieLogLoss FLOAT NOT NULL DEFAULT 0,
+    pinnacleLogLoss FLOAT NOT NULL DEFAULT 0,
+    betOnlineLogLoss FLOAT NOT NULL DEFAULT 0,
+    bet365LogLoss FLOAT NOT NULL DEFAULT 0,
+    modelLogLoss FLOAT NOT NULL DEFAULT 0,
+    PRIMARY KEY(id),
+);
