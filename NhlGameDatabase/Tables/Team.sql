b2cadbfb2d07d9f8e762cc9c3b302a624f762100
Cole <cole.titze@outlook.com>
2022-10-16 15:20:01 -0500

    Fixed error in team table

--- NhlGameDatabase/Tables/Team.sql
+++ NhlGameDatabase/Tables/Team.sql
@@ -1,7 +1,7 @@
 CREATE TABLE [dbo].[Team]
 (
     id INT NOT NULL,
-    abbreviation VARCHAR(MAX) NOT NULL
+    abbreviation VARCHAR(MAX) NOT NULL,
     locationName VARCHAR(MAX) NOT NULL,
     teamName VARCHAR(MAX) NOT NULL,
     logoUri VARCHAR(MAX) NOT NULL,
